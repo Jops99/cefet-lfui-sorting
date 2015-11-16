b0a9d45b8a6a99d3b344a768ba2580f36ff84335
Joo99 <jaopss@outlook.com>
2015-11-16 14:18:24 -0200

    Removendo espaço desnecessario

--- main.cpp
+++ main.cpp
@@ -16,46 +16,23 @@
 void GerarVetorAleatorio(unsigned long *v, unsigned long n)
 
 {
-
 	unsigned long i;
-
-
-
-
     for(i = 0; i < n; i++) {
-
         v[i] = (rand() % n);
-
     }
 }
 
-
-
-
-
 Estatisticas Ordenar(unsigned long *v, unsigned long n)
-
 {
 
-
-
 	Estatisticas estatisticas;
-
-
 
 	estatisticas.movimentacoes = 0;
 	estatisticas.comparacoes = 0;
 
-
-
 	clock_t tempo1, tempo2;
 
-
 	tempo1 = clock();
-
-
-
-
 
 	//ordena o vetor
 
@@ -64,22 +41,11 @@ Estatisticas Ordenar(unsigned long *v, unsigned long n)
 
     algoritmoEscolhido(v, n, &estatisticas.movimentacoes, &estatisticas.comparacoes);
 
-
-
-
 	tempo2 = clock();
-
-
 
 	estatisticas.tempo =  (double)(tempo2 - tempo1) / CLOCKS_PER_SEC;
 
-
-
-
-
 	return estatisticas;
-
-
 
 }
 
@@ -94,9 +60,7 @@ Estatisticas Ordenar(unsigned long *v, unsigned long n)
 int main(int argc, char **argv)
 
 {
-
     printf("|                                                    |\n");
-
 
     //pergunta tamanho do vetor
     int tamanho = 100000;
@@ -106,43 +70,20 @@ int main(int argc, char **argv)
 	//cria o vetor de acordo com o tipo de conjunto escolhido
     unsigned long* v = (unsigned long*)malloc(sizeof(unsigned long)*tamanho);
     GerarVetorAleatorio(v, tamanho);
-
     printf("| Vetor gerado                                       |\n");
-
     //estatisticas de acordo com o algoritmo escolhido
 
     printf("| Comecando a ordenar:                               |\n");
-
     Estatisticas estatisticas = Ordenar(v, tamanho);
-
-
-
-
-
 
     //Depois da ordenação
 
 
     printf("| Resultados:                                        |\n");
-
-
-
     printf("   Tempo gasto (em seg.): %f                         \n", estatisticas.tempo);
-
-
-
     printf("   Movimentacoes: %lu                                \n", estatisticas.movimentacoes);
-
-
-
     printf("   Comparacoes: %lu                                  \n", estatisticas.comparacoes);
-
-
-
     printf("|                                                    |\n\n\n");
-
-
-
 	return 0;
 }
 
